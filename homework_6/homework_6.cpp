0a6e760deecdb9e5053786d3398954b3e8339f41
Algis <algis.h@yandex.ru>
2021-07-08 10:33:01 +0300

    Выполнена домашняя работа №6.

---
+++ homework_6/homework_6.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "homework_6.h"
+
+
+
+int main(int argc, char const *argv[])
+{
+	// Task 1
+
+	std::cout << "Задание №1." << std::endl;
+	
+	task1();
+
+	std::cout << "---------------------------" << std::endl;
+
+	// Task 2
+
+	std::cout << "Задание №1." << std::endl;
+
+	std::cout << "Тест!" <<  endll;
+	std::cout << "Тест!" << std::endl;
+
+	std::cout << "---------------------------" << std::endl;
+
+	return 0;
+}
